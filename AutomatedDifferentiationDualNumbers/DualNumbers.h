6a577aca231bc0b8c855e8b441eac106c4acbbeb
Vern <111416701+vernthedragon@users.noreply.github.com>
2026-06-19 11:33:04 +0700

    Initial Push

---
+++ AutomatedDifferentiationDualNumbers/DualNumbers.h
@@ -0,0 +1,86 @@
+#pragma once
+#include <cmath>
+
+#if defined(_MSC_VER)
+#define __DUALFORCEINLINE __forceinline
+#else
+#define __DUALFORCEINLINE [[gnu::always_inline]]
+#endif
+
+template <typename Var>
+struct Dual {
+    Var re;
+    Var eps;
+    Dual(Var real = 0, Var epsilon = 0) : re(real), eps(epsilon) {};
+    template <typename VarOther>
+    Dual(Dual<VarOther> other) : re(other.re), eps(other.eps) {};
+
+    inline Dual operator+(const Dual& other) const {
+        return Dual(this->re + other.re, this->eps + other.eps);
+    }
+    inline Dual operator-(const Dual& other) const {
+        return Dual(this->re - other.re, this->eps - other.eps);
+    }
+    Dual operator*(const Dual& other) const {
+        return Dual(this->re * other.re, (this->eps * other.re) + (this->re * other.eps));
+    }
+    template <typename VarOther>
+    inline Dual operator*(const VarOther& scalar) const {
+        return Dual(this->re * static_cast<Var>(scalar), this->eps * static_cast<Var>(scalar));
+    }
+    template <typename VarOther>
+    inline Dual operator/(const VarOther& scalar) const {
+        if (scalar == 0)
+            return *this;
+        return Dual(this->re / static_cast<Var>(scalar), this->eps / static_cast<Var>(scalar));
+    }
+    inline Dual operator+(const Var& scalar) const {
+        return Dual(this->re + scalar, this->eps);
+    }
+    inline Dual operator-(const Var& scalar) const {
+        return Dual(this->re - scalar, this->eps);
+    }
+    __DUALFORCEINLINE Dual operator~() const { //conjugate operator
+        return Dual(re, -eps);
+    }
+    __DUALFORCEINLINE Dual conj() const {
+        return Dual(re, -eps);
+    }
+    Dual operator/(const Dual& other) const {
+
+        if (other.re == 0) // ##! error handling for zero division in dual
+            return *this;
+
+        return Dual(
+            this->re / other.re,
+            ((this->eps * other.re) - (this->re * other.eps)) / ((other.re * other.re))
+        );
+    }
+    __DUALFORCEINLINE bool operator==(const Dual& other) const {
+        return this->re == other.re && this->eps == other.eps;
+    }
+    __DUALFORCEINLINE bool operator!=(const Dual& other) const {
+        return !(this->operator==(other));
+    }
+    __DUALFORCEINLINE Var ang() const { return static_cast<Var>(this->eps / this->re); }
+    __DUALFORCEINLINE Var abs() const { return this->re; }
+
+};
+
+
+#ifndef __DUALHPP
+#define __DUALHPP
+template <typename Var>
+inline Dual<Var> sin(Dual<Var> number) {
+    return Dual<Var>(sin(number.re), cos(number.re));
+}
+template <typename Var>
+inline Dual<Var> cos(Dual<Var> number) {
+    return Dual<Var>(cos(number.re), -sin(number.re));
+}
+template <typename Var>
+inline Dual<Var> exp(Dual<Var> number) {
+    return Dual<Var>(exp(number.re), exp(number.re) * number.eps);
+}
+typedef Dual<double> DualNumber;
+#endif
---
+++ AutomatedDifferentiationDualNumbers/Main.cpp
@@ -0,0 +1,23 @@
+﻿// AutomatedDifferentiationDualNumbers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "DualNumbers.h"
+
+DualNumber f(DualNumber x) { //this if f(x), the function we want to differentiate at x = a
+    //we use f(x) = e^x + 3cos(x)sin(x) + x^2 + 9x^3 for this example
+    return exp(x)   +   cos(x) * sin(x) * 3   +   x * x   +   x * x * x * 9;
+}
+
+
+int main()
+{
+
+    DualNumber x = DualNumber(6.0, 1.0); // we evaluate f(x) at x = 6 + 1ϵ  (x = 6)
+    //we use 1ϵ so that the ϵ part of f(x) will be the derivative
+    //f(a+bϵ) = f(a)+bf'(a)ϵ
+    DualNumber answer = f(x);
+
+    std::cout << "f(x) = " << answer.re << ", f'(x) = " << answer.eps;
+}
+
